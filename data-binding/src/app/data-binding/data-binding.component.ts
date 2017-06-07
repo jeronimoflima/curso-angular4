85c38c98d2687eabed490159e16264e1bdfc15c1
jeronimoflima <jeronimoflima@yahoo.com.br>
2017-06-07 08:59:09 -0300

    att

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url: string = 'http://loiane.training';
+
+  cursoAngular = true;
+
+  isMouseOver: boolean = false;
+
+  urlImagem = 'http://lorempixel.com/400/200/nature/';
+
+  valorS;
+
+  getValor(){
+    return 1;
+  }
+
+  mouseOn(){
+    this.isMouseOver = !this.isMouseOver;
+
+  }
+
+  mouseOut(){
+
+  }
+
+  getCurtiCurso(){
+    return true;
+  }
+
+  botaoClicado(){
+    alert("botaoClicado");
+  }
+
+  onKeyUp(evento:KeyboardEvent){
+    console.log(evento);
+  }
+
+  valorSalvo(valor){
+    this.valorS = valor;
+
+
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
